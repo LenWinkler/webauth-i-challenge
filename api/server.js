66d1f565fc84263f9aaff9702802eb76e25a4910
LenWinkler <lenwinkler86@gmail.com>
2019-11-11 21:26:39 -0500

    filled out requires-auth-middleware.js and added it to GET endpoint

--- api/server.js
+++ api/server.js
@@ -1,13 +1,10 @@
 const express = require('express');
 
-// const ApiRouter = require('./api-router.js');
 const configureMiddleware = require('./configure-middleware.js');
 
 const server = express();
 server.use(express.json());
 
 configureMiddleware(server);
-
-// server.use('/api', ApiRouter);
 
 module.exports = server;
